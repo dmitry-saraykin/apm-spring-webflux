7ee55b1dafe584c3a0afd288397f203d531fc0b8
Dmitry Saraykin <1506231@gmail.com>
2018-08-14 08:44:07 +0300

    ignore exceptions

--- src/main/java/co/elastic/apm/filter/ElasticApmFilter.java
+++ src/main/java/co/elastic/apm/filter/ElasticApmFilter.java
@@ -11,12 +11,25 @@ import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 
+import java.nio.channels.ClosedChannelException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
 import reactor.core.publisher.Mono;
 
 public class ElasticApmFilter implements WebFilter {
     public static final String TRANSACTION_ATTRIBUTE = "ELASTIC_APM_TRANSACTION";
+    private final Set<Class<? extends Exception>> ignoreErrors;
+    
+    public ElasticApmFilter() {
+        ignoreErrors = new HashSet<Class<? extends Exception>>(Arrays.asList(ClosedChannelException.class));
+    }
+    
+    public ElasticApmFilter(Class<? extends Exception>... exceptionsToIgnore) {
+        ignoreErrors = new HashSet<Class<? extends Exception>>(Arrays.asList(exceptionsToIgnore));
+    }
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
@@ -37,11 +50,13 @@ public class ElasticApmFilter implements WebFilter {
                 .doOnSuccess(nothing -> {
                     transaction.end();
                 }).doOnError(e->{
-                    String url = request.getURI().getPath();
-                    if (!StringUtils.isEmpty(request.getURI().getQuery())) {
-                        url += "?" + request.getURI().getQuery();
+                    if (!ignoreErrors.contains(e.getClass())) {
+                        String url = request.getURI().getPath();
+                        if (!StringUtils.isEmpty(request.getURI().getQuery())) {
+                            url += "?" + request.getURI().getQuery();
+                        }
+                        ElasticApm.captureException(new ExceptionWrapper(e, action.get(), request.getMethodValue() + " " + url));
                     }
-                    ElasticApm.captureException(new ExceptionWrapper(e, action.get(), request.getMethodValue() + " " + url));
                     transaction.end();
                 }).doOnCancel(() -> {
                      transaction.end();
